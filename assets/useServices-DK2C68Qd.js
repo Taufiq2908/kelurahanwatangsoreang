ddb460921b5f68ac50abce05047cf7a306f58171
Taufiq2908 <Taufiq2908@users.noreply.github.com>
2026-07-29 13:41:13 +0000

    deploy: a1215dc93e3a57c6c22e6d00b024693efb75fbe3

--- assets/useServices-DK2C68Qd.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./useApiData-C2Odd2as.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
