f364d6c809899cfd9ddbb2decd423b836a48e8f4
Taufiq2908 <Taufiq2908@users.noreply.github.com>
2026-08-02 06:33:58 +0000

    deploy: 49c421cf14db29239c95985708de48692b17f37c

---
+++ assets/useServices-C2lpTQnL.js
@@ -0,0 +1,1 @@
+import{u as s}from"./useApiData-CupgNT6K.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
