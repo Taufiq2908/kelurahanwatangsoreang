a5ba3f21840af13fd85b2dffb317752e9ed0787c
Taufiq2908 <taufiqurrahmanhendra29@gmail.com>
2026-08-02 14:33:05 +0800

    Updates

--- assets/useServices-CRkLpL7p.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./useApiData-CPk6T1bd.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
