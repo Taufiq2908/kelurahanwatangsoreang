be1bc990387f11d45114d86eadc7ef46811b3df6
Taufiq2908 <Taufiq2908@users.noreply.github.com>
2026-08-02 06:07:09 +0000

    deploy: 950a53b444a1c73edde72c23761a8ca90cf785d3

---
+++ assets/useServices-yVXVm7o5.js
@@ -0,0 +1,1 @@
+import{u as s}from"./useApiData-6f7suNT6.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
