790e224e204d412a3820ba7ee74738cbf959e997
Taufiq2908 <taufiqurrahmanhendra29@gmail.com>
2026-08-02 14:25:15 +0800

    Updates

--- assets/useServices-yVXVm7o5.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./useApiData-6f7suNT6.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
