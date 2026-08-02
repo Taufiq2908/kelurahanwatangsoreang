e634ccfd187fc19871452c4e981709c86054883a
Taufiq2908 <Taufiq2908@users.noreply.github.com>
2026-08-02 15:33:41 +0000

    deploy: 4ad37b270fef4cef0de0f8a54428e345c628bead

---
+++ assets/useServices-DyoGBYmz.js
@@ -0,0 +1,1 @@
+import{u as s}from"./useApiData-Bv3zW-2d.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
