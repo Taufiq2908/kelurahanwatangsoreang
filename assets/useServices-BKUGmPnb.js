bb624e37d5cd6229380ba8a52ec738984e96be6b
Taufiq2908 <Taufiq2908@users.noreply.github.com>
2026-08-02 05:23:18 +0000

    deploy: f35ffb006d7d491841bf457f25dc4bc64f76b192

---
+++ assets/useServices-BKUGmPnb.js
@@ -0,0 +1,1 @@
+import{u as s}from"./useApiData-DlVvlGrg.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
