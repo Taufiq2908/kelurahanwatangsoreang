1f0e4c31d47d6b48807ced8af30f8381f39847e0
Taufiq2908 <taufiqurrahmanhendra29@gmail.com>
2026-08-02 14:43:29 +0800

    Updates

--- assets/useServices-C2lpTQnL.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./useApiData-CupgNT6K.js";function o(){const{data:e,loading:r,error:a,refetch:t}=s("getLayanan");return{services:e||[],loading:r,error:a,refetch:t}}export{o as u};
